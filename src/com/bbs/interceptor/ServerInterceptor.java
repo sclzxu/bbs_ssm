d583cbf1e97b317d872da84ee049b9b475e8ddee
Mr Xu <543376302@qq.com>
2020-05-19 10:04:42 +0800

    修改了拦截 /server/ 下面的所有请求

--- src/com/bbs/interceptor/ServerInterceptor.java
+++ src/com/bbs/interceptor/ServerInterceptor.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bbs.pojo.User;
+
 public class ServerInterceptor implements HandlerInterceptor {
 	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
@@ -20,6 +22,12 @@ public class ServerInterceptor implements HandlerInterceptor {
 		Object loginer = request.getSession().getAttribute("loginer");
 		if(null == loginer) {
 			response.sendRedirect("/bbs_ssm/login");
+			return false;
+		}
+		// 非系统管理员跳转到首页
+		User user = (User)loginer;
+		if(!user.getUserLevel().getLevelMessage().equals("系统管理员")) {
+			response.sendRedirect("/bbs_ssm/client/index");
 			return false;
 		}
 		return true;
